4090eb5995d964c4f2e89efd13906576b355d767
Daniel <daniel_oliveira110@hotmail.com>
2020-06-23 11:37:19 -0300

    Exercícios Resolvido

---
+++ Modulo 01/02.js
@@ -0,0 +1,9 @@
+função  pares ( x ,  y )  {
+    para  ( var  i  =  x ;  i <= y ;  i ++ )  {
+      if  ( i % 2  ===  0 )  {
+        console . log ( i ) ;
+      }
+    }
+  }
+  
+  pares ( 32 ,  321 ) ;
---
+++ Modulo 01/03.js
@@ -0,0 +1,6 @@
+função  temHabilidade ( habilidades )  {
+    habilidades de retorno . indexOf ( "Javascript" ) ! == - 1 ;
+ }
+ 
+ var  skills  =  [ "Javascript" ,  "ReactJS" ,  "React Native" ] ;
+ console . log ( temHabilidade ( habilidades ) ) ;
