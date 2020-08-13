48e8c79c5d9d9a8b082e2b2df48f0efb85343b92
Daniel Oliveira <daniel_oliveira110@hotmail.com>
2020-08-13 11:01:01 -0300

    Delete 03.js

--- Modulo 01/03.js
+++
@@ -1,6 +0,0 @@
-função  temHabilidade ( habilidades )  {
-    habilidades de retorno . indexOf ( "Javascript" ) ! == - 1 ;
- }
- 
- var  skills  =  [ "Javascript" ,  "ReactJS" ,  "React Native" ] ;
- console . log ( temHabilidade ( habilidades ) ) ;
