2b78b853a2898d26d6850b01527952e69990d361
Daniel Oliveira <daniel_oliveira110@hotmail.com>
2020-08-13 10:28:45 -0300

    Delete 02.js

--- Modulo 01/02.js
+++
@@ -1,9 +0,0 @@
-função  pares ( x ,  y )  {
-    para  ( var  i  =  x ;  i <= y ;  i ++ )  {
-      if  ( i % 2  ===  0 )  {
-        console . log ( i ) ;
-      }
-    }
-  }
-  
-  pares ( 32 ,  321 ) ;
